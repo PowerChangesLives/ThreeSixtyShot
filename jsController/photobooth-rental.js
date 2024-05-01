46a16786f86c4cf598f5b2f675070cdce76cab49
Shaeneno Craig <shaeneno@powerchangeslives.com>
2024-05-01 14:50:53 -0500

    Deleted jsController/photobooth-rental.js as file is no longer needed

--- jsController/photobooth-rental.js
+++
@@ -1,7 +0,0 @@
-function scrollToSection(sectionId) {
-    var section = document.getElementById(sectionId);
-    if (section) {
-        section.scrollIntoView({ behavior: 'smooth' });
-    }
-}
-//FILE NEEDS TO BE DELETED
